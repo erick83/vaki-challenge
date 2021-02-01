cfc16ad1a50061a2e4047ff8ae68e25dbf8254ce
Erick Lopez <erick83@gmail.com>
2021-02-01 17:02:47 -0400

    Adding removeFromCart functions

--- functions/index.js
+++ functions/index.js
@@ -9,13 +9,10 @@ const functions = require('firebase-functions');
 // });
 
 const admin = require('firebase-admin');
-const { access } = require('fs');
 admin.initializeApp();
 const db = admin.firestore()
 
 exports.addToCart = functions.https.onCall((data) => {
-  const docRef = db.collection('Rewards').where('key', '==', data.reward_key)
-
   return _addToCart(db, data.reward_key)
 })
 
@@ -37,11 +34,8 @@ async function _addToCart(db, reward_key) {
               db.collection('Cart').add({ reward_key, cant: 1 })
             } else {
               cartSnaptshot.forEach(cartDoc => {
-                console.log(cartDoc)
                 const cart = cartDoc.data()
-                console.log(cart)
                 cart.cant+=1
-                console.log(cart, cartDoc.id)
                 db.collection('Cart').doc(cartDoc.id).set(cart)
               })
             }
@@ -52,5 +46,42 @@ async function _addToCart(db, reward_key) {
     return e
   }
 
-  return { message: 'success' }
+  return { message: 'Add Success' }
+}
+
+exports.removeFromCart = functions.https.onCall((data) => {
+  return _removeFromCart(db, data.reward_key)
+})
+
+async function _removeFromCart(db, reward_key) {
+  const rewardsDocRef = db.collection('Rewards').where('key', '==', reward_key)
+  const cartDocRef = db.collection('Cart').where('reward_key', '==', reward_key)
+  try {
+    const snaptshot = await rewardsDocRef.get()
+
+      snaptshot.forEach(rewardsDoc => {
+        const reward = rewardsDoc.data()
+
+        if (reward.quantityAvailable > 0) {
+          reward.quantityAvailable+=1
+
+          cartDocRef.get().then(cartSnaptshot => {
+            if (!cartSnaptshot.empty) {
+              cartSnaptshot.forEach(cartDoc => {
+                const cart = cartDoc.data()
+                if (cart.cant > 0) {
+                  db.collection('Rewards').doc(rewardsDoc.id).set(reward)
+                  cart.cant-=1
+                  db.collection('Cart').doc(cartDoc.id).set(cart)
+                }
+              })
+            }
+          })
+        }
+      })
+  } catch(e) {
+    return e
+  }
+
+  return { message: 'Remove Success' }
 }
